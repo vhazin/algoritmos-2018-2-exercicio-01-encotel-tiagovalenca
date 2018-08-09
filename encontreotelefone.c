14a9e3624d14410912da733460976c996782c457
tiagovalenca <36921610+tiagovalenca@users.noreply.github.com>
2018-08-08 23:10:05 -0300

    Update encontreotelefone.c

--- encontreotelefone.c
+++ encontreotelefone.c
@@ -9,23 +9,32 @@ int main(void) {
     i = 0;
     while(numero[i] != '\0'){
       if (numero[i] == 'A' || numero[i] == 'B' || numero[i] == 'C'){
-      numero[i] = '2';
+        numero[i] = '2';
+        i++;
       } else if(numero[i] == 'D' || numero[i] == 'E' || numero[i] == 'F'){
         numero[i] = '3';
+        i++;
       } else if(numero[i] == 'G' || numero[i] == 'H' || numero[i] == 'I'){
         numero[i] = '4';
+        i++;
       } else if(numero[i] == 'J' || numero[i] == 'K' || numero[i] == 'L'){
         numero[i] = '5';
+        i++;
       } else if(numero[i] == 'M' || numero[i] == 'N' || numero[i] == 'O'){
         numero[i] = '6';
+        i++;
       } else if(numero[i] == 'P' || numero[i] == 'Q' || numero[i] == 'R' || numero[i] == 'S'){
         numero[i] = '7';
+        i++;
       } else if(numero[i] == 'T' || numero[i] == 'U' || numero[i] == 'V'){
         numero[i] = '8';
+        i++;
       } else if(numero[i] == 'W' || numero[i] == 'X' || numero[i] == 'Y' || numero[i] == 'Z'){
         numero[i] = '9';
+        i++;
+      } else{
+        i++;
       }
-      i++;
     }
     printf("%s\n", numero);
   }
