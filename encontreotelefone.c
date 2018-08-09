1ff44a6c7414d64adb5f4ba234e15229553172bc
tiagovalenca <36921610+tiagovalenca@users.noreply.github.com>
2018-08-08 22:49:03 -0300

    Update encontreotelefone.c

--- encontreotelefone.c
+++ encontreotelefone.c
@@ -3,34 +3,31 @@
 int main(void) {
 
   char numero[31] = "";
-  char numeroReal[31] = "";
   int i;
 
   while(scanf("%s", numero) != EOF){
-    for (i = 0; i <= 29; i++){
-      if (numero[i] == '0' || numero[i] == '1' || numero[i] == '-'){
-      numeroReal[i] = numero[i];
-      } else if (numero[i] == 'A' || numero[i] == 'B' || numero[i] == 'C'){
-      numeroReal[i] = '2';
+    i = 0;
+    while(numero[i] != '\0'){
+      if (numero[i] == 'A' || numero[i] == 'B' || numero[i] == 'C'){
+      numero[i] = '2';
       } else if(numero[i] == 'D' || numero[i] == 'E' || numero[i] == 'F'){
-      numeroReal[i] = '3';
+        numero[i] = '3';
       } else if(numero[i] == 'G' || numero[i] == 'H' || numero[i] == 'I'){
-      numeroReal[i] = '4';
+        numero[i] = '4';
       } else if(numero[i] == 'J' || numero[i] == 'K' || numero[i] == 'L'){
-      numeroReal[i] = '5';
+        numero[i] = '5';
       } else if(numero[i] == 'M' || numero[i] == 'N' || numero[i] == 'O'){
-      numeroReal[i] = '6';
+        numero[i] = '6';
       } else if(numero[i] == 'P' || numero[i] == 'Q' || numero[i] == 'R' || numero[i] == 'S'){
-      numeroReal[i] = '7';
+        numero[i] = '7';
       } else if(numero[i] == 'T' || numero[i] == 'U' || numero[i] == 'V'){
-      numeroReal[i] = '8';
+        numero[i] = '8';
       } else if(numero[i] == 'W' || numero[i] == 'X' || numero[i] == 'Y' || numero[i] == 'Z'){
-        numeroReal[i] = '9';
-      } else {
-        numeroReal[i] = '\0';
+        numero[i] = '9';
       }
+      i++;
     }
-    printf("%s\n", numeroReal);
+    printf("%s\n", numero);
   }
   return 0;
 }
